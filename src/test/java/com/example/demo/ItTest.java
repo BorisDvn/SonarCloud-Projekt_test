26f05f716d69e6510e6adb43df18d9c31c0972bf
BorisDvn <dovonon@th-brandenburg.de>
2021-12-08 00:19:04 +0100

    renane -it -> ItTest

--- src/test/java/com/example/demo/ItTest.java
+++ src/test/java/com/example/demo/ItTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IT {
+class ItTest {
 
 	@Test
 	void my_simple_integration_test() {
