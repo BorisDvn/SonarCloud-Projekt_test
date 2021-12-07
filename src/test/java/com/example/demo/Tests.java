478365a92e08f91adc5bd2ddcda40c5ec5179167
BorisDvn <dovonon@th-brandenburg.de>
2021-12-08 00:22:27 +0100

    add assert

--- src/test/java/com/example/demo/Tests.java
+++ src/test/java/com/example/demo/Tests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class Tests {
 
 	@Test
 	void my_simple_unit_test() {
+		Assert.isTrue(true);
 		System.out.println("This is a unit test!");
 	}
 
