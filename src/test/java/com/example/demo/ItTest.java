6af42af50dbcf09e360e35f86a9c0699cd11885d
BorisDvn <dovonon@th-brandenburg.de>
2021-12-08 15:05:56 +0100

    HelloController add name

--- src/test/java/com/example/demo/ItTest.java
+++ src/test/java/com/example/demo/ItTest.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class ItTest {
 
 	@Test
 	void my_simple_integration_test() {
-		Assert.isTrue(true);
 		System.out.println("This is a integration test!");
 	}
 
--- src/test/java/com/example/demo/Tests.java
+++ src/test/java/com/example/demo/Tests.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class Tests {
 
 	@Test
 	void my_simple_unit_test() {
-		Assert.isTrue(true);
 		System.out.println("This is a unit test!");
 	}
 
