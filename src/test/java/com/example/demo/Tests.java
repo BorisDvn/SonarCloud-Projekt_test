ff91ee00b48a637ba93f4a1349e47a168d42e5f7
BorisDvn <dovonon@th-brandenburg.de>
2021-12-08 15:44:21 +0100

    add test

--- src/test/java/com/example/demo/Tests.java
+++ src/test/java/com/example/demo/Tests.java
@@ -1,14 +1,34 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class Tests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void my_simple_unit_test() {
-		System.out.println("This is a unit test!");
+		System.out.println("This is a fake unit test!");
+	}
+
+	@Test
+	public void exampleTest() throws Exception {
+		this.mvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(content().string("Greetings from Spring Boot!"));
 	}
 
 }
