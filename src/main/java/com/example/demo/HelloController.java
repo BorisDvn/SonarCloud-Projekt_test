cbd6cbc0a4a9ea46d9e52083d3a2a1d413e0ca00
BorisDvn <dovonon@th-brandenburg.de>
2021-12-08 15:09:21 +0100

    HelloController delete name

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,17 +1,13 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Value("${spring.application.name}")
-    String name;
-
     @GetMapping("/")
     public String index() {
-        return "Greetings from" + name + "!";
+        return "Greetings from Spring Boot!";
     }
 }
